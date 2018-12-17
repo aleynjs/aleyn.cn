515234770c9429ca955db61973d4a6b261edce34
helin3 <helin3@yusys.com.cn>
2018-12-17 22:35:52 +0800

    添加导航，侧边栏配置

--- www/@/.vuepress/config.js
+++ www/@/.vuepress/config.js
@@ -3,10 +3,34 @@
  */
 module.exports = {
   base: process.env.NODE_ENV === 'production' ? './' : '/', // 部署站点的基础路径
-  title: 'Aleyn He\'s Personal Website',
+  title: 'Aleyn.CN',
   description: 'The official documentation site for Aleyn.js',
+  head: [
+    ['link', { rel: 'icon', href: '/public/favicon.ico' }]
+  ],
   host: '127.0.0.1',
   port: '1217',
+  dest: 'www/@/.vuepress/dist',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '教程', link: '/guide/' },
+      {
+        text: '笔记',
+        items: [
+          { text: '工具', link: '/note/devtools/' },
+          { text: '前端', link: '/note/fe/' },
+          { text: '后端', link: '/note/be/' }
+        ]
+      }
+    ],
+    sidebar: 'auto',
+    search: true,
+    searchMaxSuggestions: 10,
+    lastUpdated: 'Last Updated', // string | boolean
+    // 假定是 GitHub.
+    repo: 'aleynjs/aleyn.cn'
+  },
   markdown: {
     lineNumbers: false, // 配置
     // markdown-it-anchor 的选项
@@ -19,7 +43,7 @@ module.exports = {
     }
   },
   configureWebpack: (config, isServer) => {
-    // config.resolve.alias['@alias'] = 'www/@'
+    config.resolve.alias['@alias'] = process.cwd() + '/www/@/'
   },
   chainWebpack: (config, isServer) => {
     // config 是 ChainableConfig 的一个实例
--- www/@/README.md
+++ www/@/README.md
@@ -1,1 +1,16 @@
-Hello World
+---
+home: true
+heroImage: /logo.png
+heroText: Aleyn.CN
+tagline: Aleyn He's Personal Website
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 学习笔记
+  details: 记录日常工作中遇到的常见问题，以及解决方法。
+- title: 学习笔记
+  details: 记录日常工作中遇到的常见问题，以及解决方法。
+- title: 学习笔记
+  details: 记录日常工作中遇到的常见问题，以及解决方法。
+footer: MIT Licensed | Copyright © 2018 aleynjs
+---
