19780cdda4edd83a22c8e798f65baa2c40801f54
helin3 <helin3@yusys.com.cn>
2018-12-17 15:53:12 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,26 @@
 # abort on errors
 set -e
 
-# 编译文件目录：www/@，编译后输出至：www/@/.vuepress/dist/
+# 编译文件目录：www/@
 npm run build:www
 
-# 切换至分支gh-pages
-git checkout gh-pages
+# cd到编译输出目录
+cd www/@/.vuepress/dist/
 
-# 复制编译后文件至gh-pages分支根目录下
-cp -r www/@/.vuepress/dist/* ./
-
-# 部署至自定义域名
+# 如果您部署至自定义域名，则启用下述语句
+# echo 'www.example.com' > CNAME
 echo 'aleyn.cn' > CNAME
 
 # 提交本次修改
+git init
 git add -A
 git commit -m 'deploy'
 
-# push至远程仓库：github
-git push -f github gh-pages
+# 如果您部署至： https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/aleynjs/aleyn.cn.git master:gh-pages
+
+# 如果您部署至： https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 cd -
